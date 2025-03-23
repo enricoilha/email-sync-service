c4ccf62387ac2374019c80c7756bf248be127e6b
Enrico Pozzi <enricoilha@gmail.com>
2025-03-23 20:52:11 -0300

    fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('port');
+  const port = configuration().port;
 
   app.enableCors({
-    origin: configService.get<string>('corsOrigin') || '*',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
