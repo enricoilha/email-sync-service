ce01be6320e0a943b0f8bf8f72df1786ecd3f502
Enrico Pozzi <enricoilha@gmail.com>
2025-03-23 22:38:03 -0300

    fixed configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   //@ts-ignore
-  port: parseInt(process.env.PORT, 10) || 3001,
+  port: process.env.PORT || 3001,
   supabase: {
     url: process.env.SUPABASE_URL,
     serviceKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
